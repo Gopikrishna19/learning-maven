1b550f01dac8368867d5b688a5d83cdb638c53a4
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-06-16 12:13:40 -0500

    customize javadoc plugin

--- Plugins/src/main/java/HelloWorld.java
+++ Plugins/src/main/java/HelloWorld.java
@@ -1,4 +1,8 @@
 public class HelloWorld {
+    /**
+     * Prints hello world text
+     * @param args default command line args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
